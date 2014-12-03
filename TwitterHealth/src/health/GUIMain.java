bbea088381ca4a680338197cf4628392657eb5eb
Kaiyuan Shi <kaiyuan@uw.edu>
2014-12-03 15:04:19 -0800

    init

---
+++ TwitterHealth/src/health/GUIMain.java
@@ -0,0 +1,42 @@
+package health;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+/**
+
+ */
+public final class GUIMain {
+    
+    /**
+     * Private constructor to prohibit instantiation.
+     */
+    private GUIMain() { }
+
+    /**
+     * main method that starts the program.
+     * @param aRgs command line arguments, assumes none
+     */
+    public static void main(String[] aRgs) {
+        EventQueue.invokeLater(new Runnable()
+        {
+            public void run() {
+                // create a frame
+                final JFrame frame = new JFrame();
+                // create a panel
+                final TwitterHealthPanel panel = new TwitterHealthPanel();
+                // add a panel to the frame
+                frame.add(panel);
+                frame.setTitle("Twitter Health Project");
+                frame.setSize(500, 400);
+                frame.setResizable(false);
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+                
+            }
+        });
+    }
+
+}
